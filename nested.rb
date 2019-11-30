35abfce00998c46be06802c3876393f2b50270d0
bionic-chain-9657 <mywisdom3@yahoo.com>
2019-11-30 21:29:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -14,8 +14,7 @@ def hopper
           :known_for => "Unix",
           :languages => ["C"]
         }
-        programmer_hash [:grace_hopper, :known_for, :languages]
-           }
+     }
 
 end
 
