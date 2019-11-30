e91419337b4e719380ecd6f33377c38648cae8bd
bionic-chain-9657 <mywisdom3@yahoo.com>
2019-11-30 21:34:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,6 +56,10 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
         programmer_hash ={
+          :yukihiro_matsumoto => {
+            :known_for => "Ruby",
+            :languages=> ["LISP", "C"]
+        }
      }
 end
 
